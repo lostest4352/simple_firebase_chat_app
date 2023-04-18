bf47b08d31a47c091ed98b106f945407b1df1f90
user <username@email.com>
2023-04-18 17:12:19 +0545

    Initial commit. Made a simple login page

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:simple_firebase1/pages/home_page.dart';
+import 'package:simple_firebase1/pages/login_page.dart';
+
+void main() async {
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(
+        statusBarColor: Colors.black12,
+        statusBarIconBrightness: Brightness.light,
+      ),
+    );
+  }
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class DefaultFirebaseOptions {}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Simple App',
+      theme: ThemeData.dark(),
+      home: LoginPage(),
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool showPassword = false;
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    void togglevisibility() {
+      setState(() {
+        showPassword = !showPassword;
+      });
+    }
+
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                // Login Icon
+                const Icon(
+                  Icons.account_box,
+                  size: 80,
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                // Text to the user
+                Text(
+                  'Welcome back!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue.shade100,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // Email textfield
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your email',
+                    ),
+                    obscureText: false,
+                  ),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+
+                // Password textfield
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextField(
+                    controller: passwordController,
+                    obscureText: !showPassword,
+                    decoration: InputDecoration(
+                      hintText: 'Enter your password',
+                      suffixIcon: IconButton(
+                        icon: Icon(showPassword
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          togglevisibility();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+
+                // Forgot password?
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Text('Forgot Password?'),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    padding: const EdgeInsets.fromLTRB(60, 10, 60, 10),
+                    textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 15),
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    'Login',
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: const [
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.white30,
+                      ),
+                    ),
+                    Text('Or continue with'),
+                    Expanded(
+                      child: Divider(
+                        thickness: 0.5,
+                        color: Colors.white30,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30,),
+
+                // Google sign in button
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        
+                      },
+                      child: SizedBox(
+                        height: 40,
+                        child: Image.asset('assets/images/google.png'),
+                      ),
+                    ),
+                  ],
+
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

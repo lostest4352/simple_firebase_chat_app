75bda50944d11131309eb651a6c23fc613438e5a
user <username@email.com>
2023-06-21 21:24:30 +0545

    Added missed search page to git

---
+++ lib/pages/search_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Search groups or users"),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}

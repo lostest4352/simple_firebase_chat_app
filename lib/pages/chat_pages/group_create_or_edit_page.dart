852f6986c28ffff6289d8a4549343aaf2f710e2f
user <username@email.com>
2023-07-13 23:57:04 +0545

    Added a page

---
+++ lib/pages/chat_pages/group_create_or_edit_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateOrEditGroup extends StatefulWidget {
+  const CreateOrEditGroup({super.key});
+
+  @override
+  State<CreateOrEditGroup> createState() => _CreateOrEditGroupState();
+}
+
+class _CreateOrEditGroupState extends State<CreateOrEditGroup> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
